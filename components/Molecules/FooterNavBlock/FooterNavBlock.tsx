d499b255e9bed82e0924ec37d13698a0091d3d5f
B0sto <bostoganashvilinika2019@gmail.com>
2025-09-13 19:46:13 +0400

    footer responsive

--- components/Molecules/FooterNavBlock/FooterNavBlock.tsx
+++ components/Molecules/FooterNavBlock/FooterNavBlock.tsx
@@ -1,23 +1,43 @@
-import FooterText from '@/components/Atoms/FooterText/FooterText'
-import FooterTitle from '@/components/Atoms/FooterTitle/FooterTitle'
+"use client";
+import { useState } from "react";
+import FooterText from "@/components/Atoms/FooterText/FooterText";
+import FooterTitle from "@/components/Atoms/FooterTitle/FooterTitle";
+import BottomArrow from "@/public/BottomArrow";
 
 interface footerNavBlockProps {
-    title: string;
-    items: string[];
+  title: string;
+  items: string[];
 }
 
-const FooterNavBlock = ({title, items}: footerNavBlockProps) => {
+const FooterNavBlock = ({ title, items }: footerNavBlockProps) => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <div>
-        <FooterTitle title={title}/>
+    <div className="w-full lg:w-auto">
+      {/* Header */}
+      <button
+        className="flex justify-between items-center w-full lg:cursor-default"
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        <FooterTitle title={title} />
+        <BottomArrow
+          className={`w-4 h-4 transition-transform lg:hidden ${
+            isOpen ? "rotate-180" : "rotate-0"
+          }`}
+        />
+      </button>
 
-        <ul className='flex flex-col gap-y-3.5'>
-            {items.map((item, index) => (
-                <FooterText key={index} text={item}/>
-            ))}
-        </ul>
+      {/* Items */}
+      <ul
+        className={`flex flex-col gap-y-3.5 overflow-hidden transition-all duration-300 
+        ${isOpen ? "max-h-96 mt-3" : "max-h-0 lg:max-h-none lg:mt-3"}`}
+      >
+        {items.map((item, index) => (
+          <FooterText key={index} text={item} />
+        ))}
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default FooterNavBlock
+export default FooterNavBlock;
